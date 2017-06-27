3366ee7a011f0702d048215f1acd04a29091d4b2
wiziple <wiziple@gmail.com>
2017-06-27 14:33:05 -0700

    added image-webpack-loader with 2 test image assets

--- src/image-viewer.js
+++ src/image-viewer.js
@@ -1,6 +1,13 @@
+import big from '../assets/big.jpg';
+import small from '../assets/small.jpg';
 import './image-viewer.css';
 
 const image = document.createElement('img');
-image.src = 'http://lorempixel.com/400/400';
+image.src = small;
 
 document.body.appendChild(image);
+
+const bigImage = document.createElement('img');
+bigImage.src = big;
+
+document.body.appendChild(bigImage);
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ const config = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: 'build/'
   },
   module: {
     rules: [
@@ -18,6 +19,16 @@ const config = {
             loader: 'css-loader'
         }),
         test: /\.css$/
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 40000 }
+          },
+          'image-webpack-loader'
+        ]
       }
     ]
   },
